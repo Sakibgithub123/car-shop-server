ace3d189014af61d5af558fdf1dbe91b341dd4ca
unknown <sakibbangla49@gmail.com>
2023-10-21 10:25:33 -0700

    get product froem server

--- index.js
+++ index.js
@@ -37,65 +37,14 @@ async function run() {
         const result = await carCollection.insertOne(product);
         res.send(result);
     })
-    app.post('/addcarts', async(req,res)=>{
-        const product = req.body;
-        console.log(product)
-        const result = await cartCollection.insertOne(product);
-        res.send(result);
-    })
-    app.put('/addproducts/:id', async(req,res)=>{
-        const id=req.params.id;
-        const filter={_id: new ObjectId(id)}
-        const options={upsert:true}
-        const updateCar=req.body
-        const car={
-            $set:{
-                name:updateCar.name,
-                brand_name:updateCar.brand_name,
-                price:updateCar.price,
-                short_description:updateCar.short_description,
-                rating:updateCar.rating,
-                image:updateCar.image
-            }
-        }
-        const result= await carCollection.updateOne(filter,car,options)
-        res.send(result);
-    })
+
     app.get('/addproducts', async(req,res)=>{
         const cursor=carCollection.find();
         const result= await cursor.toArray();
         res.send(result)
 
-    }) 
-    app.get('/addcarts', async(req,res)=>{
-        const cursor=cartCollection.find();
-        const result= await cursor.toArray();
-        console.log(result)
-        res.send(result)
-
-    }) 
-    app.get('/addproducts/:id', async(req,res)=>{
-        const id=req.params.id;
-        const query={_id: new ObjectId(id)}
-        console.log(query)
-        const result= await carCollection.findOne(query)
-        res.send(result);
     })
-    app.get('/addproducts/:brand_name', async(req,res)=>{
-        const brand_name=req.params.brand_name;
-        const query={brand_name: new ObjectId(brand_name)}
-        console.log(query)
-        const cursor= carCollection.find(query)
-        const result= await cursor.toArray();
-        res.send(result);
-    })
-    app.delete('/addproducts/:id', async(req,res)=>{
-        const id=req.params.id;
-        const query={_id: new ObjectId(id)}
-        console.log(query)
-        const result= await carCollection.deleteOne(query)
-        res.send(result);
-    })
+   
 
    
     await client.db("admin").command({ ping: 1 });
